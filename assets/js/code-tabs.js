fb57090ca06a8c22a757ac690b4523a4914f3ed6
glipR <jgoe0001@student.monash.edu>
2021-03-26 10:35:30 +1100

    Being first post on Dynamic Programming

---
+++ assets/js/code-tabs.js
@@ -0,0 +1,37 @@
+window.onload = () => {
+    // Open all Python tabs by default.
+    var all_tablinks = document.getElementsByClassName("code-tablinks");
+    for (var j = 0; j < all_tablinks.length; j++) {
+        all_tablinks[j].className = all_tablinks[j].className.replace(" active", "");
+        if (all_tablinks[j].innerHTML.toString() === "Python") {
+            all_tablinks[j].className += " active";
+        }
+    }
+
+    var all_tabcontent = document.getElementsByClassName("code-tabcontent");
+    for (var j = 0; j < all_tabcontent.length; j++) {
+        all_tabcontent[j].style.display = "none";
+        if (all_tabcontent[j].id.indexOf("Python") !== -1) {
+            all_tabcontent[j].style.display = "block";
+        }
+    }
+};
+
+function openCodeTab(evt, contentClass, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName(contentClass);
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName(contentClass + "-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+} 
