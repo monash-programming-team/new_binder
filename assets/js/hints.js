6f5b158a0d09040460233c6708897b8afb5d8cf4
glipR <37640160+glipR@users.noreply.github.com>
2021-07-26 20:40:55 +1000

    Add hint/soln buttons.

---
+++ assets/js/hints.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+
+  hints = document.getElementsByClassName("button_unlock");
+  for (var i = 0; i < hints.length; i++) {
+    hints[i].onclick = function() {
+      var cur = this;
+      while (!cur.classList.contains("unlock")) {
+        cur = cur.parentNode;
+      }
+      cur.classList.toggle("unlock");
+    };
+  }
+
+};
